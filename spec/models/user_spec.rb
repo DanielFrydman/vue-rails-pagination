9fdf376e57408d8f936f3e9ff187b38eab673a64
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-30 17:56:11 -0300

    ✅ Creating simple user model spec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has all the attributes' do
+    attributes = %w[
+      first_name last_name birthday mother_first_name father_first_name
+    ]
+
+    attributes.each do |attribute|
+      expect(described_class.column_names).to(include(attribute))
+    end
+  end
 end
