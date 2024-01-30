27dceb30fedf476e055f0cf7f282e5da717703bd
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-30 17:56:05 -0300

    ✅ Creating simple user controller spec file

--- spec/controllers/v1/users_controller_spec.rb
+++ spec/controllers/v1/users_controller_spec.rb
@@ -3,7 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe V1::UsersController, type: :controller do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe '#GET index' do
+    subject { get(:index, params: {}) }
+
+    before do
+      10.times do
+        create(:user)
+      end
+    end
+
+    it 'returns a hash with pagy info and records' do
+      subject
+      response_body = JSON.parse(response.body)
+      expect(response_body).to(have_key('pagy'))
+      expect(response_body).to(have_key('records'))
+    end
+
+    it 'returns 200' do
+      subject
+      expect(response).to(have_http_status(:ok))
+    end
   end
 end
