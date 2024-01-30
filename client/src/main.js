c6551b576acec910df0f73a30aaa23857873d723
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-30 06:43:08 -0300

    ✨ Importing API Client in the project

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,14 @@
+import ApiClient from "./api/axios.js";
 import Router from "./router/index.js";
-import App from './App.vue'
+import App from './App.vue';
 
 import { createApp } from 'vue'
 
 const app = createApp(App);
+app.mixin({
+  beforeCreate() {
+    this.$apiClient = ApiClient();
+  },
+});
 app.use(Router);
 app.mount('#app');
