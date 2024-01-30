cd5e0d27ffab68c8d92d691f3730eb320ce967f1
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-30 17:35:25 -0300

    💄 Formating date and adding width and height

--- client/src/components/UserCard.vue
+++ client/src/components/UserCard.vue
@@ -1,9 +1,9 @@
 <!-- src/components/Card.vue -->
 <template>
-  <div class="bg-white max-w-sm rounded overflow-hidden shadow-lg m-4">
+  <div class="bg-white max-w-sm rounded overflow-hidden shadow-lg m-4 w-64 h-54">
     <div class="px-6 py-4">
       <div class="font-bold text-xl mb-2">{{ user.first_name }} {{ user.last_name }}</div>
-      <div class="font-bold">Birthday: <p class="text-gray-700 text-base">{{ user.birthday }}</p>
+      <div class="font-bold">Birthday: <p class="text-gray-700 text-base">{{ formatDate(user.birthday) }}</p>
       </div>
       <div class="font-bold">Mother name: <p class="text-gray-700 text-base">{{ user.mother_first_name }}</p>
       </div>
@@ -21,5 +21,15 @@ export default {
       required: true,
     },
   },
+  methods: {
+    formatDate(date) {
+      const changedDate = this.user.birthday.replace(/(..)\/(..)\/(....) (..):(..)/, '$3-$2-$1 $4:$5');
+      var date = new Date(changedDate);
+      const day = date.getDate() + 1;
+      const month = date.toLocaleString('default', { month: 'long' });
+      const year = date.getFullYear();
+      return `${day} of ${month} of ${year}`;
+    }
+  }
 };
 </script>
