55a97a1bbcde018bcfe697cc67e64d5953bbeb21
Daniel Frydman <danielfrydman@id.uff.br>
2024-01-30 17:12:25 -0300

    ✨ Implementing home with components

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,43 @@
-<script setup>
-</script>
-
 <template>
-  <div>
+  <div class="flex flex-wrap justify-center">
+    <UserCard v-for="user in users" :key="user.id" :user="user" />
+  </div>
+  <div class="flex flex-wrap justify-center mb-10">
+    <Pagination :currentPage="currentPage" :totalPages="totalPages" @pageChange="fetchPaginatedDta" />
   </div>
 </template>
+
+<script>
+import { Pagination, UserCard } from '../components/index';
+
+export default {
+  components: {
+    Pagination, UserCard
+  },
+  data() {
+    return {
+      currentPage: 1,
+      totalPages: null,
+      users: []
+    };
+  },
+  created() {
+    this.fetchPaginatedDta()
+  },
+  methods: {
+    async fetchPaginatedDta(pageNumber) {
+      try {
+        pageNumber = pageNumber !== undefined ? pageNumber : '1';
+        const { data: { pagy, records } } = await this.$apiClient.get(`/users?page=${pageNumber}`, { withCredentials: true });
+        this.currentPage = pagy.page;
+        this.totalPages = pagy.pages;
+        this.users = records
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  }
+};
+</script>
 
 <style scoped></style>
